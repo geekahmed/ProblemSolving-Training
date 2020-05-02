507ebb99044bb336cc174fba595a1a23616dfc0f
geekahmed <geekahmed1@gmail.com>
2020-05-02 21:41:32 +0200

    Anton and Danik

--- 01.Complexity of Algorithms/734A.cpp
+++ 01.Complexity of Algorithms/734A.cpp
@@ -3,3 +3,28 @@ E-mail: geekahmed1@gmail.com
 LinkedIn: https://www.linkedin.com/in/geekahmed/
 Problem-Link: http://codeforces.com/problemset/problem/734/A
 */
+#include <iostream>
+using namespace std;
+int main() {
+    int nGames;
+    string games;
+    cin >> nGames >> games;
+    int countA = 0, countD = 0 ;
+    for(int i = 0 ; i < games.length(); i++){
+        if (games[i] == 'A'){
+            countA++;
+        } else {
+            countD++;
+        }
+    }
+    if (countA > countD){
+        cout << "Anton" << endl;
+    } else if (countD > countA){
+        cout << "Danik" << endl;
+    } else {
+        cout << "Friendship" << endl;
+    }
+    
+    return 0;
+}
+
